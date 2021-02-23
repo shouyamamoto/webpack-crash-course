f1a37ce38e42bd010df75bfc86ecd1a11df23913
shou yamamoto <yamasyo0017@gmail.com>
2021-02-24 07:38:34 +0900

    implement css-loader and style-loader

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import _ from 'lodash'
-import { nijou } from './utilities'
+import './style.css'
 
 function component() {
   const element = document.createElement('div')
   const array = ['hello', 'webpack', '!']
   element.innerHTML = _.join(array, ' ')
   return element
-}             
-
-console.log(nijou(10));
+}
 
 document.body.appendChild(component())
+
+document.body.classList.add('haikei')
