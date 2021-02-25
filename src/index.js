1ae1c93cbe0fed7e949fbb398c82dc6530e06969
shou yamamoto <yamasyo0017@gmail.com>
2021-02-26 07:22:42 +0900

    install eslint-loader

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React from 'react'
-import ReactDom from 'react-dom'
-import './style.css'
-import './style.scss'
+import React from 'react';
+import ReactDom from 'react-dom';
+import './style.css';
+import './style.scss';
 
 ReactDom.render(
   <div>Hello, React</div>,
-  document.getElementById('root')
-)
+  document.getElementById('root'),
+);
--- webpack.config.js
+++ webpack.config.js
@@ -1,61 +1,76 @@
-const HtmlWebPackPlugin = require('html-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin')
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin');
+const stylishFormatters = require('eslint/lib/cli-engine/formatters/stylish');
+const path = require('path');
 
-const path = require('path')
-const outputPath = path.resolve(__dirname, 'dist')
+const outputPath = path.resolve(__dirname, 'dist');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
     filename: 'main.js',
-    path: outputPath
+    path: outputPath,
   },
   module: {
     rules: [
       {
+        enforce: 'pre', // preを持たないローダーよりも早く実行する
+        test: /\.jsx?$/, // jsまたはjsxを対象とする
+        exclude: /node_modules/, // nodeは対象外にする
+        use: [
+          {
+            loader: 'eslint-loader',
+            options: {
+              formatter: stylishFormatters,
+            },
+          },
+        ],
+      },
+      {
         test: /\.(sc|c)ss$/,
         use: [
-          MiniCssExtractPlugin.loader, 
+          MiniCssExtractPlugin.loader,
           'css-loader',
-          'sass-loader'
-        ]
+          'sass-loader',
+        ],
       },
       {
         test: /\.(jpe?g|png|svg|ico)$/i,
         loader: 'url-loader',
         options: {
           limit: 2048,
-          name: './images/[name].[ext]'
-        }
+          name: './images/[name].[ext]',
+        },
       },
       {
         test: /\.m?jsx?$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env']
-          }
-        }
+            presets: ['@babel/preset-env'],
+          },
+        },
       },
       {
         test: /\.html$/,
-        loader: "html-loader"
-      }
-    ]
+        loader: 'html-loader',
+      },
+    ],
   },
   devServer: {
-    contentBase: outputPath
+    contentBase: outputPath,
   },
   plugins: [
     new HtmlWebPackPlugin({
       template: './src/index.html',
-      filename: './index.html'
+      filename: './index.html',
     }),
     new MiniCssExtractPlugin({
       filename: '[name].[hash].css',
-    })
+    }),
   ],
   optimization: {
     minimizer: [
@@ -63,4 +78,4 @@ module.exports = {
     ],
   },
   devtool: 'eval-source-map',
-}
+};
