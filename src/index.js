4618f05e43b9988240470f769a425b7fefb18507
shou yamamoto <yamasyo0017@gmail.com>
2021-02-25 19:12:13 +0900

    applay mini-css-extract-plugin

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+import './style.css'
+import './style.scss'
 
 ReactDom.render(
   <div>Hello, React</div>,
   document.getElementById('root')
 )
-
